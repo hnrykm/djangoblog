42180b1c36f5dd25950acc9cab94ba643b2139a9
Henry Kim <hnrykm@gmail.com>
2023-04-26 15:44:48 -0400

    Added models, class views, urls, and templates

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish"),
+) # tuple to keep draft and published posts separate, referenced in "status"
+
+class Post(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(
+        User,
+        related_name="blog_posts",
+        on_delete=models.CASCADE,
+    )
+    updated_on = models.DateTimeField(auto_now=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    class Meta:
+        ordering = ["-created_on"] # negative indicates reverse order, so recenty appears first
+
+    def __str__(self):
+        return self.title # used on the admin panel
+    
