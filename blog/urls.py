78869fdef1896e29c2195328499b6efc7d70b677
Henry Kim <hnrykm@gmail.com>
2023-04-27 11:39:02 -0400

    Added Post - Create, Edit, Delete

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,11 @@
-from blog import views
 from django.urls import path
-from blog.views import posts_list, post_details
+from blog.views import posts_list, post_details, new_post, edit_post, delete_post
 
 urlpatterns = [
     path("", posts_list, name="home"),
-    path("<slug:slug>/", post_details, name="post_detail"),
-    path("new_post/", views.NewPost.as_view(), name="new_post"),
+    path("<slug:slug>/edit/", edit_post, name="edit_post"),
+    path("<slug:slug>/delete_post/", delete_post, name="delete_post"),
+    path("new_post/", new_post, name="new_post"),
+    path("<slug:slug>/", post_details, name="post_detail")
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,3 @@
-from django.views import generic
 from django.shortcuts import render, get_object_or_404, redirect
 from blog.models import Post
 from django.contrib.auth.decorators import login_required
@@ -19,18 +18,41 @@ def post_details(request, slug):
     }
     return render(request, "post_detail.html", context)
 
-# class PostDetail(generic.DetailView):
-#     model = Post
-#     template_name = "post_detail.html"
+@login_required
+def new_post(request):
+    if request.method == "POST":
+        form = NewPostForm(request.POST)
+        if form.is_valid():
+            post = form.save(False)
+            post.author = request.user
+            post.save()
+            return redirect("home")
+    else:
+        form = NewPostForm()
+    context = {
+        "form": form,
+    }
+    return render(request, "new_post.html", context)
 
-class NewPost(generic.CreateView):
-    model = Post
-    fields = [
-        "title",
-        "author",
-        "content",
-        "status",
-    ]
-    template_name = "new_post.html"
+def edit_post(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+    if request.method == "POST":
+        form = NewPostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect("post_detail", slug=slug)
+    else:
+        form = NewPostForm(instance=post)
+    context = {
+        "post": post,
+        "form": form,
+    }
+    return render(request, "edit_post.html", context)
 
-    
+
+def delete_post(request, slug):
+    post = Post.objects.get(slug=slug)
+    if request.method == "POST":
+        post.delete()
+        return redirect("home")
+    return render(request, "delete_post.html")
